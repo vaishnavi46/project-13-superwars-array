545dbf9eb838da6d2a8b745a33e53910fa65d936
vaishnavi46 <vaishunivi12@gmail.com>
2020-08-14 00:30:19 +0530

    Prograd 1537699

--- src/app.js
+++ src/app.js
@@ -23,13 +23,30 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
-    // Instead of forloop use Map method
-    // Code here
-
+    let detailedPlayers = [];
+    // Instead of forloop use Map method    // Code here
+    detailedPlayers = players.map(function (name, index) {
+        var details = {
+            name: players[index],
+            strength: getRandomStrength(),
+            img: "images/super-" + (index + 1) + ".png", 
+        }
+        if ((index + 1) % 2 == 0) 
+        {
+            details.type = "hero";
+        } 
+        else 
+        {
+            details.type = "villain";
+        }
+        return details;
+    });
+    //console.log(detailedPlayers);
     return detailedPlayers;
 }
+            
+    // Instead of forloop use Map method
+    // Code here
 
 // getting random strength
 const getRandomStrength = () => {
@@ -37,16 +54,33 @@ const getRandomStrength = () => {
 }
 
 // Build player template
+// Instead of using for loop
+// Use chaining of Array methods - filter, map and join
+// Type your code here
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = [];
+    let filtered = [];
+    let final=[];
+    let finalstring='';
+    fragment = players.map(function(item, index)
+    {
+        return item;
+    }
+    );
+    filtered = fragment.filter(function (item,index)
+    {
+        return item.type==type;
+    }
+    );
+    final=filtered.map(function(item,index)
+    {
+        finalstring=finalstring+'<div class="player"><img src='+item.img+'><div class="name">'+item.name+'</div><div class="strength">'+item.strength+'</div></div>';
+        return item;
+    })
 
-    // Instead of using for loop
-    // Use chaining of Array methods - filter, map and join
-    // Type your code here
+    return finalstring;
 
-    return fragment;
 }
-
 // Display players in HTML
 const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
